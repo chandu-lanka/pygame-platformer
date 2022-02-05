9f97a816fef3e778d2b19921448890ae5ec052ad
TriangleDevv <chandu.lanka13@gmail.com>
2022-02-05 19:40:05 +0530

    tutorial-1 code

--- main.py
+++ main.py
@@ -1,1 +1,38 @@
-# pygame platformer
+import pygame,sys 
+from pygame.locals import *
+from scripts.settings import *
+
+# basic setup
+pygame.init()
+screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption("platformer")
+fps = 60
+
+player = pygame.Rect(10, 10, 50, 50)
+rect = pygame.Rect(300, 10, 100, 50)
+color = (0, 255, 0)
+
+# game loop
+while True:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_RIGHT]:
+        player.x += 3
+    elif keys[pygame.K_LEFT]:
+        player.x -= 3
+
+    if player.colliderect(rect):
+        color = (0, 0, 255)
+    else:
+        color = (0, 255, 0)
+
+
+    screen.fill((0, 0, 0))
+    pygame.draw.rect(screen, (255, 0, 0), player, 3)
+    pygame.draw.rect(screen, color, rect, 3)
+    pygame.display.update()
+    clock.tick(fps)
---
+++ scripts/settings.py
@@ -0,0 +1,5 @@
+import pygame
+
+screen_width = 1200
+screen_height = 700
+clock = pygame.time.Clock()
